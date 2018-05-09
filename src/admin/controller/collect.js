357f06b6f472339eb0c53dd450327b5f4fad8fff
Jang <25720297@qq.com>
2018-05-10 01:02:48 +0800

    add iconv-lite

--- src/admin/controller/collect.js
+++ src/admin/controller/collect.js
@@ -1,31 +1,45 @@
 const Base = require('./base.js');
 const cheerio = require('cheerio');
 const request = require('request');
+const iconv = require('iconv-lite');
+
 const dataUrl = 'https://m.pc6.com/xcx/all.html';
 
 const requestAction = function (url) {
   return new Promise( function (resolve, reject) {
-    request({url: url, method: 'GET'}, function (error, response, body) {
+    request({url: url, method: 'GET', encoding:null}, function (error, response, body) {
+      // response.setEncoding('utf-8');
+      console.log(response);
       if (error) return reject(error);
-
-      resolve(body);
+      const buf =  iconv.decode(body, 'gb2312');
+      resolve(buf);
     });
   });
 };
 
 module.exports = class extends Base {
   indexAction() {
-
-    return this.success('aaa');
+    this.requestCollect();
+    
+    
+    // this.assign('index',xx);
+    return this.display();
+    // return this.success('aaa');
   }
 
   testAction() {
-    this.requestCollect();
-    // return this.success(bodyT);
+    // const b = this.requestCollect();
+
+    // return this.success(b.toString());
   }
 
   async requestCollect() {
     const a = await requestAction('https://m.pc6.com/xcx/all.html');
     console.log(a);
+    const $ = cheerio.load(a);
+    console.log($)
+    const xx = $('#cateAll').length;
+    console.log(xx);
+    return a;
   }
 };
